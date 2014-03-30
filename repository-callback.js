304a15f933448de563510cca4c26c4beba9c7d6a
crocket <crockabiscuit@gmail.com>
2014-03-30 22:32:42 +0900

    repository-callback.js : Refactor pull_webmail

--- repository-callback.js
+++ repository-callback.js
@@ -13,15 +13,22 @@ module.exports={
 
 function pull_webmail(dir) {
   // Spawn a new git process.
-  var args="-u www-data -g www-data -H git pull".split(" ");
-  var proc=spawn("/usr/bin/sudo", args, {
-    "cwd" : dir,
-    "env" : process.env
-  });
+  var cmd="/usr/bin/sudo -u www-data -g www-data -H git pull";
+  var proc=spawnCommand(cmd, dir);
 
   proc.on('exit', function (code, signal) {
     console.log(sprintf("git pull on %s exited with code %d", dir, code));
   });
 
   return proc;
+}
+
+function spawnCommand(cmd, dir) {
+  var args=cmd.split(" ");
+  cmd=args.splice(0, 1)[0];
+
+  return spawn(cmd, args, {
+    "cwd": dir,
+    "env" : process.env
+  });
 }
