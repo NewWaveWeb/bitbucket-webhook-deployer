73a63ca30d8025b58e4e06b09afb253b3868d1b5
crocket <crockabiscuit@gmail.com>
2014-03-30 23:21:11 +0900

    Remove verbse logging.

--- main.js
+++ main.js
@@ -3,19 +3,10 @@ var repoCb=require("./repository-callback");
 var gitlabhook = require('gitlabhook');
 
 var gitlab = gitlabhook({
-  "port" : config.port,
-  "logger" : {
-    "info" : function (data) {
-      console.log("==INFO==");
-      console.log(data);
-    },
-    "error" : function (data) {
-      console.log("==ERROR==");
-      console.log(data);
-    }
-  }
+  "port" : config.port
 }, function (data) {
   var repo=data.repository.name;
+  
   // If repository is defined in repository-config.js, execute it.
   if(repoCb[repo]) {
     repoCb[repo]();
